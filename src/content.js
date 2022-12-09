86b3debbf7b4cb3516a4ad3d44e73a9d0e1af789
Luciana Santos <luciydss@hotmail.com>
2022-12-09 10:38:19 -0300

    Create content.js

---
+++ src/content.js
@@ -0,0 +1,24 @@
+export const content = {
+  filterItems: [
+    {
+      id: Math.round(Math.random() * 99999),
+      name: 'All',
+    },
+    {
+      id: Math.round(Math.random() * 99999),
+      name: 'Flowers',
+    },
+    {
+      id: Math.round(Math.random() * 99999),
+      name: 'Pots',
+    },
+    {
+      id: Math.round(Math.random() * 99999),
+      name: 'Plants',
+    },
+    {
+      id: Math.round(Math.random() * 99999),
+      name: 'Cactus',
+    },
+  ],
+};
